4abbf2a0db87f1a1d36a848010926d7607f9908e
Alexander Hart <alexanderhart@Alexanders-MacBook-Pro.local>
2024-06-21 17:55:36 -0400

    Updates

---
+++ precache-manifest.7256cf8f01d037df7c39157f549a17dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6e7db437d67967fb612340f333b3ae",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1d9c35449cdfd9f05cd7",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "4040a8b06009e7836fef",
+    "url": "/portfolio/static/css/main.ad924fcf.chunk.css"
+  },
+  {
+    "revision": "1d9c35449cdfd9f05cd7",
+    "url": "/portfolio/static/js/2.fda1ec09.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.fda1ec09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4040a8b06009e7836fef",
+    "url": "/portfolio/static/js/main.ba78bee5.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
